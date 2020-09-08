11696f1b5b3c96874b5f88b8fc1de4b418b91b69
Ketan <ketantoo@gmail.com>
2020-09-08 12:50:49 +0530

    Loading screen is added with spinkit library

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:brew_app_flutter_firebase/services/auth.dart';
 import 'package:brew_app_flutter_firebase/shared/constants.dart';
+import 'package:brew_app_flutter_firebase/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -16,89 +17,102 @@ class _RegisterState extends State<Register> {
 
   String email = "";
   String password = "";
+  bool loading = false;
 
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text("Register to Brew Crew"),
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: widget.toggleView,
-              icon: Icon(Icons.person),
-              label: Text("Sign in"),
-            )
-          ]),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 20.0,
-          horizontal: 50.0,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(children: <Widget>[
-            SizedBox(
-              height: 20.0,
-            ),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: "Email"),
-              validator: (value) {
-                return value.isEmpty ? "Enter an Email" : null;
-              },
-              onChanged: (val) {
-                setState(
-                  () {
-                    email = val;
-                  },
-                );
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: "Password"),
-              validator: (value) {
-                return value.length < 6
-                    ? "Enter password longer than 6 char"
-                    : null;
-              },
-              obscureText: true,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              color: Colors.pink[400],
-              child: Text(
-                "Register",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+                backgroundColor: Colors.brown[400],
+                elevation: 0.0,
+                title: Text("Register to Brew Crew"),
+                actions: <Widget>[
+                  FlatButton.icon(
+                    onPressed: widget.toggleView,
+                    icon: Icon(Icons.person),
+                    label: Text("Sign in"),
+                  )
+                ]),
+            body: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 50.0,
               ),
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  await _auth.registerWithEmailPassword(
-                    email: email,
-                    password: password,
-                  );
-                }
-              },
+              child: Form(
+                key: _formKey,
+                child: Column(children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: "Email"),
+                    validator: (value) {
+                      return value.isEmpty ? "Enter an Email" : null;
+                    },
+                    onChanged: (val) {
+                      setState(
+                        () {
+                          email = val;
+                        },
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration:
+                        textInputDecoration.copyWith(hintText: "Password"),
+                    validator: (value) {
+                      return value.length < 6
+                          ? "Enter password longer than 6 char"
+                          : null;
+                    },
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        setState(() {
+                          loading = true;
+                        });
+                        dynamic result = await _auth.registerWithEmailPassword(
+                          email: email,
+                          password: password,
+                        );
+
+                        if (result == null) {
+                          setState(() {
+                            loading = false;
+                          });
+                        }
+                      }
+                    },
+                  ),
+                  // Text("Email: ${email}\nPassword: ${password}")
+                ]),
+              ),
             ),
-            // Text("Email: ${email}\nPassword: ${password}")
-          ]),
-        ),
-      ),
-    );
+          );
   }
 }
--- lib/screens/authenticate/signin.dart
+++ lib/screens/authenticate/signin.dart
@@ -1,5 +1,6 @@
 import 'package:brew_app_flutter_firebase/services/auth.dart';
 import 'package:brew_app_flutter_firebase/shared/constants.dart';
+import 'package:brew_app_flutter_firebase/shared/loading.dart';
 import "package:flutter/material.dart";
 
 class SignIn extends StatefulWidget {
@@ -18,84 +19,97 @@ class _SignInState extends State<SignIn> {
 
   String email = "";
   String password = "";
+  bool loading = false;
 
   final _signinFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text("Sign in to Brew Crew"),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: widget.toggleView,
-            icon: Icon(Icons.person),
-            label: Text("Register"),
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 20.0,
-          horizontal: 50.0,
-        ),
-        child: Form(
-          key: _signinFormKey,
-          child: Column(children: <Widget>[
-            SizedBox(
-              height: 20.0,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text("Sign in to Brew Crew"),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: widget.toggleView,
+                  icon: Icon(Icons.person),
+                  label: Text("Register"),
+                )
+              ],
             ),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: "Email"),
-              validator: (value) => value.isEmpty ? "Enter an Email" : null,
-              onChanged: (val) {
-                setState(() {
-                  email = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: "Password"),
-              validator: (value) => value.length < 6
-                  ? "Enter password longer than 6 chars"
-                  : null,
-              obscureText: true,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              color: Colors.pink[400],
-              child: Text(
-                "Sign in",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+            body: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 50.0,
               ),
-              onPressed: () async {
-                if (_signinFormKey.currentState.validate()) {
-                  await _auth.signInWithEmailPassword(
-                    email: email,
-                    password: password,
-                  );
-                }
-              },
+              child: Form(
+                key: _signinFormKey,
+                child: Column(children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: "Email"),
+                    validator: (value) =>
+                        value.isEmpty ? "Enter an Email" : null,
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration:
+                        textInputDecoration.copyWith(hintText: "Password"),
+                    validator: (value) => value.length < 6
+                        ? "Enter password longer than 6 chars"
+                        : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_signinFormKey.currentState.validate()) {
+                        setState(() {
+                          loading = true;
+                        });
+                        dynamic result = await _auth.signInWithEmailPassword(
+                          email: email,
+                          password: password,
+                        );
+                        if (result == null) {
+                          setState(() {
+                            loading = false;
+                          });
+                        }
+                      }
+                    },
+                  ),
+                  // Text("Email: ${email}\nPassword: ${password}")
+                ]),
+              ),
             ),
-            // Text("Email: ${email}\nPassword: ${password}")
-          ]),
-        ),
-      ),
-    );
+          );
   }
 }
